2ef95906663bd3362505d0f4df891dbca7d5386e
morispolanco <morispolanco@gmail.com>
2023-09-26 04:30:13 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,6 @@
 import streamlit as st
 import requests
+import docx2txt
 
 # Cambiar el título en la pestaña del navegador
 st.set_page_config(page_title="DeepLTranslate", layout="centered")
@@ -31,8 +32,8 @@ st.title("DeepLTranslate")
 st.markdown("## La mejor traducción automática del mundo")
 st.markdown("Las redes neuronales de DeepLTranslate son capaces de captar hasta los más mínimos matices y reproducirlos en la traducción a diferencia de cualquier otro servicio. Para evaluar la calidad de nuestros modelos de traducción automática, realizamos regularmente pruebas a ciegas. En las pruebas a ciegas, los traductores profesionales seleccionan la traducción más precisa sin saber qué empresa la produjo. DeepLTranslate supera a la competencia por un factor de 3:1.")
 
-# Texto a traducir
-text = st.text_input("Ingrese el texto a traducir:", value="Hello, world!")
+# Cargar archivo DOCX
+uploaded_file = st.file_uploader("Cargar archivo DOCX", type=["docx"])
 
 # Selección de idioma de destino
 target_lang = st.selectbox("Seleccione el idioma de destino:", ["DE", "ES"])
@@ -42,7 +43,10 @@ auth_key = st.text_input("Ingrese su clave de autenticación de DeepL:")
 
 # Botón para traducir
 if st.button("Traducir"):
-    if auth_key:
+    if auth_key and uploaded_file is not None:
+        # Leer el contenido del archivo DOCX
+        text = docx2txt.process(uploaded_file)
+
         translation, detected_source_language = translate_text(text, target_lang, auth_key)
         if translation:
             st.success(f"Traducción: {translation}")
@@ -50,4 +54,4 @@ if st.button("Traducir"):
         else:
             st.error("Error al traducir el texto. Verifique su clave de autenticación o intente nuevamente.")
     else:
-        st.error("Por favor, ingrese una clave de autenticación de DeepL válida.")
+        st.error("Por favor, cargue un archivo DOCX y ingrese una clave de autenticación de DeepL válida.")
